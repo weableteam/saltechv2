f304cc2ed07cc675ddccd7ed49105892f2d6ca47
ngoisao962 <luongvanchuong963@gmail.com>
2023-05-19 10:23:53 +0700

    goodbye

--- template-parts/blocks/sections/w-imageText.php
+++ template-parts/blocks/sections/w-imageText.php
@@ -31,7 +31,32 @@ if( !empty($block['align']) ) {
 // Load values and assign defaults.
 ?>
 <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
-    <div class="text-center">Image Text</div>
+    <div class="container img_text_box">
+        <div class="mt-4">
+        <div class="container">
+            <div class="w-textimg" style="background-image: url('<?=  get_stylesheet_directory_uri() . '/assets/images/br_w_vl.png' ?>')">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="w-textimg-content">
+                            <h3 class="w-textimg-title">
+                                TẠO SAO SALTECH BẮT ĐẦU
+                            </h3>
+                            <p class="w-textimg-p">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta bibendum sem. In vitae mi gravida, tempus neque eu, interdum risus. Ut tincidunt diam sed dolor mattis sollicitudin. Donec aliquam laoreet diam id vehicula. Suspendisse odio lorem, fermentum ac justo sit amet, fermentum tristique ligula. Nam fringilla auctor nunc id pulvinar.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="w-textimg_peopole">
+                            <img src="<?=  get_stylesheet_directory_uri() . '/assets/images/item_pepole.png' ?>" alt="" class="">
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 </section>
 <?php 
 //add_action( 'wp_footer', 'itScripts', 99, 1 );
--- template-parts/blocks/sections/w-values.php
+++ template-parts/blocks/sections/w-values.php
@@ -31,7 +31,64 @@ if( !empty($block['align']) ) {
 // Load values and assign defaults.
 ?>
 <section id="<?php echo esc_attr($id); ?>" class="<?php echo esc_attr($className); ?>">
-    <div class="text-center">Values</div>
+<div class="container">
+                <p class="w-vl-p_title">
+                    OUR VALUES
+                </p>
+                <div class="w-vl-content-box" style="background-image: url('<?=  get_stylesheet_directory_uri() . '/assets/images/br_w_vl.png' ?>')" >
+                    
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="w-vl-item active text-center">
+                                <img src="<?=  get_stylesheet_directory_uri() . '/assets/images/item_icon.png' ?>" class="icon-item_img" alt="">
+                                <h4 class="w-vl-h4-title">
+                                    giá trị 1
+                                </h4>
+                                <p class="w-vl-title-p">
+                                    Lorem ipsum dolor sit amet consectetur . Accusamus asperiores officiis facere aliquam iure, obcaecati totam aspernatur nesciunt quia?
+                                </p>
+                                <p class="w-vl-title-p">
+                                    Accusamus asperiores officiis facere aliquam iure, obcaecati totam aspernatur nesciunt quia?
+                                </p>
+                            </div>
+                            
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="w-vl-item  text-center">
+                                <img src="<?=  get_stylesheet_directory_uri() . '/assets/images/item_icon.png' ?>" class="icon-item_img" alt="">
+                                <h4 class="w-vl-h4-title">
+                                    giá trị 1
+                                </h4>
+                                <p class="w-vl-title-p">
+                                    Lorem ipsum dolor sit amet consectetur . Accusamus asperiores officiis facere aliquam iure, obcaecati totam aspernatur nesciunt quia?
+                                </p>
+                                <p class="w-vl-title-p">
+                                    Accusamus asperiores officiis facere aliquam iure, obcaecati totam aspernatur nesciunt quia?
+                                </p>
+                            </div>
+                            
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="w-vl-item  text-center">
+                                <img src="<?=  get_stylesheet_directory_uri() . '/assets/images/item_icon.png' ?>" class="icon-item_img" alt="">
+                                <h4 class="w-vl-h4-title">
+                                    giá trị 1
+                                </h4>
+                                <p class="w-vl-title-p">
+                                    Lorem ipsum dolor sit amet consectetur . Accusamus asperiores officiis facere aliquam iure, obcaecati totam aspernatur nesciunt quia?
+                                </p>
+                                <p class="w-vl-title-p">
+                                    Accusamus asperiores officiis facere aliquam iure, obcaecati totam aspernatur nesciunt quia?
+                                </p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            
+          
+       
+    </div>
 </section>
 <?php 
 //add_action( 'wp_footer', 'vlScripts', 99, 1 );
